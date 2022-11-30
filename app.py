7da7ad85d3c8f89227af9c5670f6ef1ac5672f58
xinj3 <xinj3@illinois.edu>
2022-11-29 23:04:24 -0600

    add server

---
+++ app.py
@@ -0,0 +1,31 @@
+from bartender_backend import *
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+bartender = Bartender("dataset/cocktail_all.csv")
+bartender.load_analizer()
+
+@app.route("/", methods=["GET", "POST"])
+def by_mix():
+    if request.method == "GET":
+        json_message = request.get_json()
+        return bartender.get_recommendation_list(json_message["user_input"], k=10).to_json()
+
+@app.route("/by_ingredients", methods=["GET", "POST"])
+def by_ingredients():
+    if request.method == "GET":
+        json_message = request.get_json()
+        return bartender.get_recommendation_list_by_ingredient(json_message["user_input"], k=5).to_json()
+
+
+@app.route("/by_name", methods=["GET", "POST"])
+def by_name():
+    if request.method == "GET":
+        json_message = request.get_json()
+        return bartender.get_recommendation_list_by_name(json_message["user_input"], k=5).to_json()
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
