eb07a72a7a7cefa04f25b6b2bcea51e678266f64
xinj3 <xinj3@illinois.edu>
2022-12-01 13:53:11 -0600

    add cors

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from bartender_backend import *
 from flask import Flask, request, jsonify
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 
@@ -7,23 +8,26 @@ bartender = Bartender("dataset/cocktail_all.csv")
 bartender.load_analizer()
 
 @app.route("/", methods=["GET", "POST"])
+@cross_origin()
 def by_mix():
-    if request.method == "GET":
+    if request.method == "POST":
         json_message = request.get_json()
-        return bartender.get_recommendation_list(json_message["user_input"], k=json_message["rec_num"])
+        return bartender.get_recommendation_list(json_message["user_input"])
 
 @app.route("/by_ingredients", methods=["GET", "POST"])
+@cross_origin()
 def by_ingredients():
-    if request.method == "GET":
+    if request.method == "POST":
         json_message = request.get_json()
-        return bartender.get_recommendation_list_by_ingredient(json_message["user_input"], k=json_message["rec_num"])
+        return bartender.get_recommendation_list_by_ingredient(json_message["user_input"])
 
 
 @app.route("/by_name", methods=["GET", "POST"])
+@cross_origin()
 def by_name():
-    if request.method == "GET":
+    if request.method == "POST":
         json_message = request.get_json()
-        return bartender.get_recommendation_list_by_name(json_message["user_input"], k=json_message["rec_num"])
+        return bartender.get_recommendation_list_by_name(json_message["user_input"])
 
 
 
